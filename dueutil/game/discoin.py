9cff0ba403224e4380df1e50180fd381c14a9197
Ben Maxwell <benjamin.maxwell@student.manchester.ac.uk>
2017-08-15 02:15:26 +0100

    Fixed discoin.

--- dueutil/game/discoin.py
+++ dueutil/game/discoin.py
@@ -69,7 +69,7 @@ async def unprocessed_transactions():
                 return json.loads(result)
 
 
-@tasks.task(timeout=300111111111111111111)
+@tasks.task(timeout=300)
 async def process_transactions():
     util.logger.info("Processing Discoin transactions.")
     try:
